7555cbb7fbd1e8f9ee10a690142bb4824ed635d5
travisneto <travisneto@live.com>
2024-11-19 21:57:09 -0500

    Updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,50 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
 
 # Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+    
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    age = models.IntegerField()
+    age = models.IntegerField(default=0)
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
+    
+
+class Feeding(models.Model):
+    date = models.DateField('Feeeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0],
+        )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,86 @@
-from django.shortcuts import render
-from .models import Cat
-
-# Import HttpResponse to send text-based responses
-
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
+from .forms import FeedingForm
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home.html')
+from .models import Cat
 
 def about(request):
     return render(request, 'about.html')
 
-
+#Cats index
 def cat_index(request):
     cats = Cat.objects.all()
-    # Render the cats/index.html template with the cats data
     return render(request, 'cats/index.html', {'cats': cats})
+
+#single cat view
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat,
+        'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have,
+        })
+
+
+def associate_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+    cat = Cat.objects.get(id=cat_id)
+    toy = Toy.objects.get(id=toy_id)    
+    cat.toys.remove(toy)
+    return redirect('cat-detail', cat_id=cat_id)
+    
+
+
+#add-feeding
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
+
+
+#Cat creat class
+class CatCreate(CreateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    # success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model= Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
